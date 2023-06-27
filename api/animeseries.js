6f494ddb94dcc065048581deec071537c60927e3
hernando0811 <hernanmartinez9815@gmail.com>
2023-06-27 12:53:31 -0400

    backend finished

--- api/animeseries.js
+++ api/animeseries.js
@@ -15,5 +15,39 @@ try {
 
 })
 
+router.get("/:id", async(req, res, next) => {
+    try {
+       const singleSHowID = req.params.id
+
+       const singleSHow = await animeShows.findByPk(singleSHowID);
+
+       res.send(singleSHow);
+        
+    } catch (error) {
+        next(error);
+        
+    }
+})
+
+router.post('/', async(req, res, next) => {
+    try {
+     const {name, creator, characters, seasons, imageUrl} =  req.body
+     const newShow = await animeShows.create({
+        name: name,
+        creator: creator,
+        characters: characters,
+        seasons: seasons,
+        imageUrl: imageUrl
+     })
+
+     res.send(newShow);
+        
+    } catch (error) {
+        next(error);
+        
+    }
+
+})
+
 
 module.exports = router
--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const morgan = require("morgan");
-PORT = 3000;
+const express = require("express"); // framework that handles http requests at different url paths(routes)
+const morgan = require("morgan"); //logging middleware 
+PORT = 3000; //the port number we are listeneing to 
 
 const app = express();
 
-app.use(morgan('tiny'));
-
-app.use(require("./api/index"))
+app.use(morgan('tiny')); //logging middleware
+app.use(express.urlencoded({extended: false})); //this is what is used to parse the body and send in data to the server ("post ")
+app.use(require("./api/index"))  
 
 app.listen(3000, ()=>{
     console.log(`started listening on port:${PORT}`)
